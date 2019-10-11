f8353ad084a77aada3c1a33edd7ab9c82e8a5318
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-10-11 13:39:53 -0700

    working on routes

--- server.js
+++ server.js
@@ -4,19 +4,23 @@ const bodyParser = require('body-parser')
 const port = 8000
 const knex = require('./db/knex')
 
+let usersService = require('./users/usersService')
+
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  res.send('Api works....')
+  res.json('Api works....')
 })
 
 app.get('/api/users', (req, res) => {
-  knex.raw('SELECT * FROM users').then(function (users) {
-    res.json(users.rows)
-  })
+  usersService.getAllUsers(res)
+})
+
+app.get('/api/users/:id', (req, res) => {
+  usersService.getOneUser(req, res)
 })
 
 
